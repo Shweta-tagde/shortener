1dd2a4bd4ed80ba33bce4fb1fd888f2b204ceda4
shweta <shweta6296@gamil.com>
2022-02-11 10:33:26 +0530

    modify index

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
     <div >
    <GlobalStyle/>
       <Head>
-        <title>SHORTENER</title>
+        <title>SHORTENERR</title>
         <meta name="description" content="created url shortener easy to use" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
         <link rel="icon" href="/favicon.ico" />
